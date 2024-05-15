252c07fb72388786766d406a2b38bfb1869b2608
julieta <julietabelenp2002@gmail.com>
2024-05-15 20:18:06 -0300

    change ui in detailView components

--- src/components/detail/DetailCard.vue
+++ src/components/detail/DetailCard.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="container">
-        <div>
+        <div class="image-container">
+            <img src="@/assets/images/imageProduct.png" alt="">
+        </div>
+        <div class="text-container">
             <h1 class="arsenica">Anillo Calabria Golden</h1>
-            <p>Aros de plata inflada con baño de oro 18k. El tamaño es de 28mm y su peso es ligero. No produce alergia.</p>
-            <h3 >$9650</h3>
+            <p>Aros de plata inflada con baño de oro 18k. El tamaño es de 28mm y su peso es ligero. No produce alergia.
+            </p>
+            <h3>$9650</h3>
             <h5>3 cuotas sin interés de $3216,3</h5>
             <h6>Ver formas de pago</h6>
-        </div>
-        <div>
         </div>
     </div>
 
@@ -17,14 +19,25 @@
 
 </script>
 
-<style>
-h1{
+<style scoped>
+.container {
+    padding-top: 11rem;
+    display: flex;
+}
+
+.text-container {
+    padding-left: 2rem;
+}
+
+h1 {
     font-weight: 600;
 }
-.p{
+
+.p {
     font-weight: 600;
 }
-.h5{
+
+.h5 {
     text-decoration: underline;
 }
 </style>
