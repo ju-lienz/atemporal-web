74f341a2bdc95a6ace9d7294be198007049afcc6
Julieta Perez <100885196+ju-lienz@users.noreply.github.com>
2024-05-16 08:28:29 -0300

    improve ui design in detailCard component

--- src/components/detail/DetailCard.vue
+++ src/components/detail/DetailCard.vue
@@ -7,25 +7,32 @@
         </div>
         <div class="text-container">
             <h1 class="arsenica">Anillo Calabria Golden</h1>
-            <p>Aros de plata inflada con baño de oro 18k. El tamaño es de 28mm y su peso es ligero. No produce alergia.
+            <p class="detail">Aros de plata inflada con baño de oro 18k. El tamaño es de 28mm y su peso es ligero. No
+                produce alergia.
             </p>
-            <h3>$9650</h3>
-            <h5>3 cuotas sin interés de $3216,3</h5>
-            <h6>Ver formas de pago</h6>
+            <h3 class="price">$9650</h3>
+            <h5 class="fees">3 cuotas sin interés de $3216,3</h5>
+            <h6 class="payment">Ver formas de pago</h6>
+            <button>
+                <ShoppingCartIcon /> Añadir al carrito
+            </button>
+            <p class="cost-shipping">Costo de envío:</p>
+            <input type="text">
+            <h6 class="postal-code">No sé mi código postal</h6>
         </div>
     </div>
 
 </template>
 
-<script>
-
+<script setup>
+import ShoppingCartIcon from '@/assets/icons/ShoppingCartIcon.vue';
 </script>
 
 <style scoped>
 .container {
     padding-top: 11rem;
     display: flex;
-    padding-bottom: 11rem ;
+    padding-bottom: 11rem;
 }
 
 .image-container {
@@ -59,17 +66,81 @@
 
 .text-container {
     padding-left: 2rem;
+    width: 40%;
+}
+
+.payment {
+    font-size: 1rem;
+    text-decoration: underline;
+    font-family: 1.5rem;
+    color: #423F3E;
+    padding-bottom: 2rem;
 }
 
 h1 {
     font-weight: 600;
+    color: #423F3E;
 }
 
-p {
+.detail {
     font-weight: 400;
+    color: #423F3E;
 }
 
 h6 {
     text-decoration: underline;
+    font-family: 1.5rem;
+    color: #423F3E;
+    padding-bottom: 2rem
+}
+
+.price {
+    padding: 1.5rem 0rem 1.5rem 0rem;
+    font-size: 2.3rem;
+    font-weight: 600;
+    color: #423F3E;
+}
+
+.fees {
+    margin-bottom: 1rem;
+    font-size: 1.3rem;
+    color: #423F3E;
+}
+
+input {
+    height: 3rem;
+    width: 100%;
+    font-size: 1rem;
+    max-width: 500px;
+    padding: 1rem;
+    border-radius: .5rem;
+    background-color: transparent;
+    border: 1px solid #593122;
+    margin: 0;
+    outline: none;
+}
+
+button {
+    width: 100%;
+    padding: 0.5rem;
+    background-color: #6e3c2a;
+    color: #FBFBFB;
+    border: none;
+    border-radius: 0.3rem;
+    display: flex;
+    justify-content: center;
+    gap: .5rem;
+    cursor: pointer;
+}
+
+.cost-shipping {
+    margin-top: 1.3rem;
+    margin-bottom: 0.2rem;
+}
+
+.postal-code {
+    color: #847E7A;
+    font-size: 0.9rem;
+    margin-top: 0.3rem;
 }
 </style>
