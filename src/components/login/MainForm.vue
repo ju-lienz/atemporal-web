226ae739e549435caf29ed2fc373791dee4c5bd1
julieta <julietabelenp2002@gmail.com>
2024-05-22 17:09:17 -0300

    add tailwind to login view

--- src/components/login/MainForm.vue
+++ src/components/login/MainForm.vue
@@ -1,5 +1,39 @@
 <template>
-    <div class="container">
+
+    <div class="w-full max-w-xs container">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="sendForm">
+            <h1>Iniciar sesión</h1>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-medium mb-2" for="username">
+                    E-mail:
+                </label>
+                <input
+                    :class="`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`"
+                    id="username" type="text" placeholder="Username" v-model="email">
+            </div>
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-medium mb-2" for="password">
+                    Contraseña:
+                </label>
+                <input
+                    class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    id="password" type="password" placeholder="******************">
+                <p class="text-red-500 text-xs italic">Please choose a password.</p>
+            </div>
+            <div class="flex items-center justify-between">
+                <button
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit">
+                    Sign In
+                </button>
+                <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                    Forgot Password?
+                </a>
+            </div>
+        </form>
+
+    </div>
+    <!-- <div class="container">
         <div class="form">
             <h1>Iniciar sesión</h1>
             <p>Compra más rápido y lleva el control de tus pedidos de la manera mas cómoda.</p>
@@ -10,10 +44,38 @@
             <h6 class="forgot-password">Olvidé mi contraseña</h6>
             <button>Iniciar sesión</button>
         </div>
-    </div>
+    </div> -->
 </template>
 
 <script>
+/* import { onBeforeMount, onMounted, ref } from 'vue';
+
+onBeforeMount(() => {
+    errores.value = {
+        email: false,
+        pass: false
+    }
+
+})
+
+const errores = ref({
+    email: false,
+    pass: false
+})
+
+const email = ref("")
+const password = ref("")
+
+const sendForm = () => {
+    if (!email.includes("@")) {
+
+        errores.value = {
+            ...errores.value,
+            email: true
+        }
+    }
+    if (errores.email || errores.pass) return
+} */
 </script>
 
 <style scoped>
