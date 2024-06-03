c4a35702f535b911c5d713e60edae77f7bd73667
Julieta Perez <100885196+ju-lienz@users.noreply.github.com>
2024-06-03 11:56:11 -0300

    add categoryCards to homeview

--- src/components/home/CategoryCard.vue
+++ src/components/home/CategoryCard.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="container overflow-hidden">
         <div
-            class="relative w-full mx-auto h-64 transition duration-500 hover:scale-105 hover:transform-origin-top-left">
-            <img class="h-64 w-full object-cover" :src="imageSrc" alt="Random image">
-            <div class="absolute inset-0 bg-black opacity-40 rounded-md"></div>
+            class="relative max-h-full max-w-full transition duration-500 hover:scale-105 hover:transform-origin-top-left">
+            <img class="h-64 w-full object-cover" :src="imageSrc" alt="Product image">
+            <div class="absolute inset-0 bg-black opacity-40"></div>
             <div class="absolute inset-0 flex items-center justify-center">
                 <h2 class="text-white text-3xl font-light">{{ text }}</h2>
             </div>
         </div>
     </div>
 </template>
-
 
 <script setup>
 import { defineProps, ref, watchEffect } from 'vue';
@@ -19,7 +18,6 @@ const props = defineProps({
     image: String,
     text: String
 })
-
 
 const imageSrc = ref()
 watchEffect(async () => {
--- src/components/home/ProductCategories.vue
+++ src/components/home/ProductCategories.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="grid grid-cols-4 grid-rows-2 gap-0">
-        <div class="col-span-2 row-span-2">
+    <div class="grid grid-cols-4 grid-rows-4 gap-0">
+        <div class="col-span-2 row-span-4">
             <CategoryCard :text="cardMarroquineria.text" :image="cardMarroquineria.image" />
         </div>
-        <div class="col-span-2 col-start-3">Relojes</div>
-        <div class="col-start-3 row-start-2">Joyas</div>
-        <div class="col-start-4 row-start-2">Accesorios</div>
+        <div class="col-span-2 row-span-2 col-start-3">
+            <CategoryCard :text="cardRelojes.text" :image="cardRelojes.image" />
+        </div>
+        <div class="row-span-2 col-start-3 row-start-3">
+            <CategoryCard :text="cardJoyas.text" :image="cardJoyas.image" />
+        </div>
+        <div class="row-span-2 col-start-4 row-start-3">
+            <CategoryCard :text="cardAccesorios.text" :image="cardAccesorios.image" />
+        </div>
     </div>
 </template>
 
@@ -17,7 +23,20 @@ const cardMarroquineria = {
     text: 'Marroquineria',
 };
 
+const cardRelojes = {
+    image: 'relojes-categoria.jpg',
+    text: 'Relojes',
+};
 
+const cardJoyas = {
+    image: 'Joyas-categoria.jpg',
+    text: 'Joyas',
+};
+
+const cardAccesorios = {
+    image: 'Accesorios-categoria.jpg',
+    text: 'Accesorios',
+};
 
 </script>
 
