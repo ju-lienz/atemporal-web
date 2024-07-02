3c94315284d9f4a8af72e290ebb281a55b35ca32
Julieta Perez <julietabelenp2002@gmail.com>
2024-07-02 20:29:50 -0300

    improve ui in cartView

--- src/components/cart/Product.vue
+++ src/components/cart/Product.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="border rounded-md text-gray-600 border-gray-700/60 p-2 gap-4 flex w-full mb-4">
+    <div class="border rounded-md text-gray-600 border-gray-400/60 p-2 gap-4 flex w-full mb-4">
         <img src="@/assets/images/imageProduct.png" alt="" class="w-[10%] aspect-[14/16] object-cover rounded-md">
         <div class="w-5/12 ">
             <h4 class=" font-semibold">{{ product.producto_nombre }}</h4>
-            <!-- <p class="text-sm">Descripcion del producto ekisde</p> -->
+            <!-- <p class="text-sm">Descripcion del producto</p> -->
             <button class="text-sm absolute bottom-0 underline cursor-pointer">Eliminar</button>
         </div>
         <div class="flex items-start w-2/12 mt-2">
@@ -15,7 +15,7 @@
                 class="bg-primary  hover:bg-secondary transition aspect-square w-8 text-white rounded-full flex items-center justify-center cursor-pointer">
                 +</p>
         </div>
-        <h5 class="font-semibold text-xl w-3/12 px-4 text-right mt-2">{{ product.producto_precio }}</h5>
+        <h5 class="font-semibold text-xl w-3/12 px-4 text-right mt-2">${{ product.producto_precio }}</h5>
     </div>
 </template>
 <script setup>
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="container pt-24 mb-4 flex gap-4">
         <div class="w-8/12">
-            {{ store.count }}
-            <h2 class="flex gap-2">
+            <h2 class="flex gap-2 mb-5">
                 <ShoppingCartIcon color="#3d3d3d" width=24 />Mi carrito
             </h2>
             <Product v-for="(product, index) in store.listProduct" :key="index" :product="product" />
-
+            <div class="max-w-lg">
+                <h2>Resumen de mi orden</h2>
+                <label class="block text-gray-700 text-sm font-medium mb-2" for="username">
+                    Cupón de descuento
+                </label>
+                <input
+                    class="border-0 p-3 outline-zinc-400 block w-full text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 rounded-md"
+                    type="text" placeholder="Ingrese su cupón">
+                <h2>Total:</h2>
+                <button class="bg-primary py-2 px-12 text text-white rounded-md">Finalizar compra</button>
+                <div class="m-10"></div>
+            </div>
         </div>
     </div>
 </template>
@@ -19,10 +29,8 @@ const store = useCartStore();
 
 const products = [
     {
-
     }
 ]
-
 
 </script>
 <style scoped></style>
