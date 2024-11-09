65ab42849ab80106f7355821b32b5d8fc700c56e
Bruno Alejandro Gorosito <125034173+bruno-gorosito@users.noreply.github.com>
2024-11-09 17:36:36 -0300

    Update authStore.js

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -4,17 +4,23 @@ import { defineStore } from "pinia";
 import { computed, ref } from "vue";
 
 export const useAuthStore = defineStore("auth", () => {
-  const user = ref(null);
+  const user = ref(JSON.parse(localStorage.getItem("user")) || null);
   const token = ref(recuperarToken());
+
+
+  obtenerUsuario();
 
   async function obtenerUsuario() {
     try {
       const response = await ClienteAxios.get("Clientes/Datos");
-      user.value = response.data.cliente 
+      user.value = response.data.cliente;
+      localStorage.setItem("user", JSON.stringify(user.value));
     } catch (e) {
-      user.value =  null
+      user.value = null;
+      localStorage.removeItem("user");
     }
   }
+
 
   async function register(formData) {
     const res = await ClienteAxios.post('Clientes/Crear', formData);
@@ -23,8 +29,6 @@ export const useAuthStore = defineStore("auth", () => {
       await obtenerUsuario();
     } 
   }
-
-  obtenerUsuario();
 
   return { user, token, obtenerUsuario, register };
 });
