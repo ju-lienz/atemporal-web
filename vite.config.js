c3a43c79019874be1b583db28aadf6e569351dd0
Bruno Alejandro Gorosito <125034173+bruno-gorosito@users.noreply.github.com>
2024-11-26 20:11:49 -0300

    arreglando imagenes

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     VueDevTools(),
   ],
+  build: {
+    assetsDir: 'src/assets',
+    copyPublicDir: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
